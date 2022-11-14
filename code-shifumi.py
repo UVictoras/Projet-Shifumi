6eb712d06ea37e8d66142ca2398322be7581522a
VictorMartinan <vmartinan@gaming.tech>
2022-11-14 13:05:40 +0100

    Avec l'erreur d'input

--- code-shifumi.py
+++ code-shifumi.py
@@ -6,7 +6,7 @@ from random import choice
 
 #On admet qu'une fonction 'random' existe et que son exécution génère aléatoirement 0, 1 ou 2.
 def random():
-    lst = [1,2,3]
+    lst = [0,1,2]
     return  choice(lst)
 
 #On admet qu'une fonction 'input' existe et qu'elle récupère la réponse à ce qu'on demande à l'utilisateur dans le terminal de commande.
@@ -37,38 +37,45 @@ def Shifumi(BO):
         PlayerChoice = input("Enter what you want to play here : ")
         #On assigne à une variable IAChoice l'élément du tableau IAPossibilities correspondant à l'index du retour de l'exécution de la fonction random
         IAChoice = IAPossibilites[random()]
-        #Si le choix du joueur est différent de celui de l'IA
-        if PlayerChoice != IAChoice:
+        #Si le choix du joueur est différent de "Rock", "Paper" ou "Scissors"
+        if (PlayerChoice == "Rock") or (PlayerChoice == "Paper") or (PlayerChoice == "Scissors")
             #Alors :
-            #Si le joueur a fait "Rock" ET l'IA "Scissors" ou le joueur fait "Scissors" ET l'IA "Paper" ou le joueur fait  "Paper" et l'IA "Rock"
-            if (PlayerChoice == "Rock" and IAChoice == "Scissors") or (PlayerChoice == "Scissors" and IAChoice == "Paper") or (PlayerChoice == "Paper" or IAChoice == "Rock"):
+            #On affiche un message d'erreur et on saute le tour
+            print("Um... How to say that... You didn't input a correct answer for the game. But nevermind that, try again mate.")
+        #Sinon :
+            #Alors :
+            #Si le choix du joueur est différent de celui de l'IA
+            if PlayerChoice != IAChoice:
                 #Alors :
-                #On ajoute 1 à la valeur de ScorePlayer
-                ScorePlayer += 1
-                #On affiche le message "You got this one, great job !"
-                print("You got this one, great job !")
+                #Si le joueur a fait "Rock" ET l'IA "Scissors" ou le joueur fait "Scissors" ET l'IA "Paper" ou le joueur fait  "Paper" et l'IA "Rock"
+                if (PlayerChoice == "Rock" and IAChoice == "Scissors") or (PlayerChoice == "Scissors" and IAChoice == "Paper") or (PlayerChoice == "Paper" or IAChoice == "Rock"):
+                    #Alors :
+                    #On ajoute 1 à la valeur de ScorePlayer
+                    ScorePlayer += 1
+                    #On affiche le message "You got this one, great job !"
+                    print("You got this one, great job !")
+                #Sinon
+                else:
+                    #Alors :
+                    #On ajoute 1 à la valeur de ScoreIA
+                    ScoreIA += 1
+                    #On affiche le message "Sadly for you, he gets you on this one. Keep it up !"
+                    print("Sadly for you, he gets you on this one. Keep it up !")
             #Sinon
             else:
                 #Alors :
-                #On ajoute 1 à la valeur de ScoreIA
-                ScoreIA += 1
-                #On affiche le message "Sadly for you, he gets you on this one. Keep it up !"
-                print("Sadly for you, he gets you on this one. Keep it up !")
-        #Sinon
-        else:
-            #Alors :
-            #On affiche le message "Waw ! A draw ! That's kind of lucky, or unlucky it depends how you see that."
-            print("Waw ! A draw ! That's kind of lucky, or unlucky it depends how you see that.")
-        #Si le score du joueur est supérieur à celui du score de l'IA
-        if ScorePlayer > ScoreIA:
-            #Alors :
-            #On assigne à la variable HighestScore la valeur de ScorePlayer
-            HighestScore = ScorePlayer
-        #Sinon
-        else:
-            #Alors :
-            #On assigne à la variable HighestScore la valeur de ScoreIA
-            HighestScore = ScoreIA
+                #On affiche le message "Waw ! A draw ! That's kind of lucky, or unlucky it depends how you see that."
+                print("Waw ! A draw ! That's kind of lucky, or unlucky it depends how you see that.")
+            #Si le score du joueur est supérieur à celui du score de l'IA
+            if ScorePlayer > ScoreIA:
+                #Alors :
+                #On assigne à la variable HighestScore la valeur de ScorePlayer
+                HighestScore = ScorePlayer
+            #Sinon
+            else:
+                #Alors :
+                #On assigne à la variable HighestScore la valeur de ScoreIA
+                HighestScore = ScoreIA
     #Si le score du joueur est supérieur à celui du score de l'IA
     if ScorePlayer > ScoreIA:
         #Alors :
